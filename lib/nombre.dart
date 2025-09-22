98838eab2234a69daf48d29198f783fbd1167f16
JArturoCruz <jorge_beltran_cruz@hotmail.com>
2025-09-22 13:43:33 -0700

    hice cambios en el codigo de nombre.dart para que funcione igual pero con diferente codigo, tambien refactoricé

--- lib/nombre.dart
+++ lib/nombre.dart
@@ -1,32 +1,60 @@
-class LongitudMinimaException extends Error {}
-class LongitudMaximaException extends Error {}
-class CaracteresInvalidosException extends Error {}
-class EmpiezaConNumeroException extends Error {}
+class ErrorLongitudMinima implements Exception {
+  @override
+  String toString() => 'El texto es demasiado corto.';
+}
 
-const LongitudMinimaPermitida = 5;
-const LongitudMaximaPermitida = 15;
+class ErrorLongitudMaxima implements Exception {
+  @override
+  String toString() => 'El texto supera la longitud máxima permitida.';
+}
+
+class ErrorCaracterInvalido implements Exception {
+  @override
+  String toString() => 'Se encontraron caracteres no válidos.';
+}
 
 class Nombre {
-  late String candidato;
+  late final String valor;
 
-  Nombre(String c) {
-    if (c.length < LongitudMinimaPermitida) {
-      throw LongitudMinimaException();
+  Nombre(String input) {
+    if (!_tieneLongitudValida(input)) {
+      if (input.length < minCaracteres) {
+        throw ErrorLongitudMinima();
+      } else {
+        throw ErrorLongitudMaxima();
+      }
     }
 
-    if (c.length > LongitudMaximaPermitida) {
-      throw LongitudMaximaException();
+    if (!_todosCaracteresValidos(input)) {
+      throw ErrorCaracterInvalido();
     }
 
-    if (RegExp(r'^\d').hasMatch(c)) {
-      throw EmpiezaConNumeroException();
+    if (!_iniciaConLetra(input)) {
+      throw ErrorCaracterInvalido();
     }
 
-    final regExp = RegExp(r'^[a-zA-ZñÑ0-9]+$');
-    if (!regExp.hasMatch(c)) {
-      throw CaracteresInvalidosException();
-    }
-
-    candidato = c;
+    valor = input;    
   }
+
+
+  bool _tieneLongitudValida(String txt) =>
+      txt.length >= minCaracteres && txt.length <= maxCaracteres;
+
+  bool _todosCaracteresValidos(String texto) {
+   return texto.split('').every((char) => caracteresValidos.contains(char));
+  }
+
+
+
+  bool _iniciaConLetra(String texto) {
+    final primer = texto[0];
+    return !digitos.contains(primer);
+  }
+
+  // Constantes
+  static const  digitos = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  static const  caracteresValidos =
+      'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789';
+  static const  minCaracteres = 5;
+  static const  maxCaracteres = 30;
 }
--- test/Nombre_test.dart
+++ test/Nombre_test.dart
@@ -3,21 +3,21 @@ import 'package:test/test.dart';
 
 void main() {
   test('debe tener un minimo de longitud', () {
-expect(()=> Nombre(''),throwsA(isA<LongitudMinimaException>()));
+expect(()=> Nombre(''),throwsA(isA<ErrorLongitudMinima>()));
   }
   );
    test('debe tener un maximo de 15 de longitud', () {
-expect(()=> Nombre('EsteNombreEsMuyLargo'),throwsA(isA<LongitudMaximaException>()));
+expect(()=> Nombre('EsteNombreEsMuyLargooooooooooooooooooo'),throwsA(isA<ErrorLongitudMaxima>()));
   }
   );
 
   test('No debe tener caraceres especiales', () {
-expect(()=> Nombre('Arturo##'),throwsA(isA<CaracteresInvalidosException>()));
+expect(()=> Nombre('Arturo##'),throwsA(isA<ErrorCaracterInvalido>()));
   }
   );
 
   test('No debe empezar con numero', () {
-expect(()=> Nombre('19Arturo'),throwsA(isA<EmpiezaConNumeroException>()));
+expect(()=> Nombre('19Arturo'),throwsA(isA<ErrorCaracterInvalido>()));
   }
   );
 
